8280fe676fe078658ee95543f3c67cd0448294ee
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-12-02 19:59:23 -0700

    put back the code i accidentally deleted in the last commit

--- POVdisplay5x7/POVdisplay5x7.ino
+++ POVdisplay5x7/POVdisplay5x7.ino
@@ -369,3 +369,118 @@ byte Zer[7][6] =
  {M,M,_,_,M,_},
  {M,_,_,_,M,_},
  {_,M,M,M,_,_}};
+
+void display(byte letter[7][6]) {
+  for (int t = 0; t < 6; t++) {
+    for (int i = 0; i < 7; i++) {
+      digitalWrite(i + 2, letter[i][t]);
+    }
+    delay(10);
+  }
+  delay(5);
+}
+
+void displayLetter(char c) {
+  c = toupper(c);
+  switch (c) {
+    case 'A':
+      display(A); break;
+    case 'B':
+      display(B); break;
+    case 'C':
+      display(C); break;
+    case 'D':
+      display(D); break;
+    case 'E':
+      display(E); break;
+    case 'F':
+      display(F); break;
+    case 'G':
+      display(G); break;
+    case 'H':
+      display(H); break;
+    case 'I':
+      display(I); break;
+    case 'J':
+      display(J); break;
+    case 'K':
+      display(K); break;
+    case 'L':
+      display(L); break;
+    case 'M':
+      display(lM); break;
+    case 'N':
+      display(N); break;
+    case 'O':
+      display(O); break;
+    case 'P':
+      display(P); break;
+    case 'Q':
+      display(Q); break;
+    case 'R':
+      display(R); break;
+    case 'S':
+      display(S); break;
+    case 'T':
+      display(T); break;
+    case 'U':
+      display(U); break;
+    case 'V':
+      display(V); break;
+    case 'W':
+      display(W); break;
+    case 'Y':
+      display(X); break;
+    case 'Z':
+      display(Z); break;
+    case '!':
+      display(Ex); break;
+    case '?':
+      display(Qu); break;
+    case ',':
+      display(Co); break;
+    case '.':
+      display(Pe); break;
+    case ' ':
+      display(Sp); break;
+    case '1':
+      display(One); break;
+    case '2':
+      display(Two); break;
+    case '3':
+      display(Thr); break;
+    case '4':
+      display(Fou); break;
+    case '5':
+      display(Fiv); break;
+    case '6':
+      display(Six); break;
+    case '7':
+      display(Sev); break;
+    case '8':
+      display(Eig); break;
+    case '9':
+      display(Nin); break;
+    case '0':
+      display(Zer); break;
+  }
+}
+
+void displayString(String s) {
+  //for (int i = 0; i < sizeof(s); i++) {
+  for (int i = 0; i < 6 || i < sizeof(s); i++) {
+    displayLetter(s[i]);
+  }
+}
+
+void setup() {
+  for (int i = 2; i < 9; i++) {
+    pinMode(i, OUTPUT);
+  }
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  displayString("test ");
+}
